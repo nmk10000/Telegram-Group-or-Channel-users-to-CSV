5744ed8ef006c6f1ea3df629b85f02b70ea750c0
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-07-11 11:11:41 +0530

    Update telethon-bot-add-users-to-groups.py

--- telethon-bot-add-users-to-groups.py
+++ telethon-bot-add-users-to-groups.py
@@ -11,9 +11,9 @@ import random
 import re
 
 
-api_id = 000000        # YOUR API_ID
-api_hash = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'        # YOUR API_HASH
-phone = '+34000000000'        # YOUR PHONE NUMBER, INCLUDING COUNTRY CODE
+api_id = 1625501        # YOUR API_ID
+api_hash = '602a3b5bf8c45a4f40b71e88a32a6525'        # YOUR API_HASH
+phone = '+14149099119'        # YOUR PHONE NUMBER, INCLUDING COUNTRY CODE
 client = TelegramClient(phone, api_id, api_hash)
 
 client.connect()
